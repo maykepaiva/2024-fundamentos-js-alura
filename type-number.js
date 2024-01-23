d750643a7cb36b6211eee4f5fdbbd3ddee87abee
Mayke Gabriel Antunes de Paiva <mgadepaiva@rd.com.br>
2024-01-23 13:41:21 -0300

    iniciar em js, tipos number e string

---
+++ type-number.js
@@ -0,0 +1,17 @@
+// tipo number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 7;
+
+const operacaoMatematica=primeiroNumero+segundoNumero;
+
+console.log(operacaoMatematica);
+// para rodar em terminal utilize node e o nome do arquivo
+
+// ponto flutuante
+const numeroPontoFlutuante = 3.3
+const numeroPontoFlutuanteSemZero = .5
+
+console.log(numeroPontoFlutuante+numeroPontoFlutuanteSemZero)
---
+++ type-string.js
@@ -0,0 +1,10 @@
+const texto1 = "ola mundo"
+const texto2 = 'ola mundo'
+
+const citacao = 'eu sou "grooth"'
+
+console.log(citacao)
+
+//concatenação de string
+
+console.log(citacao + texto1 + texto2)
