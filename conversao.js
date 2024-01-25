ef36996a5321afd0e85c99d71e16134ec5495eeb
Mayke Gabriel Antunes de Paiva <mgadepaiva@rd.com.br>
2024-01-25 16:57:58 -0300

    modelos de conversao de string para number

---
+++ conversao.js
@@ -0,0 +1,13 @@
+//tipos de dados booleano
+
+//conversao implicita
+
+const numero = 456
+const numeroString = '456'
+
+console.log(numero===numeroString)
+console.log(numero + Number(numeroString))
+
+//conversao explicita
+
+// console.log(String(numero)===String(numeroString))
